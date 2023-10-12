eed5dd8f368eb1a6e103501a0c39157744426d1f
Daniil Rose <daniil.rose@posteo.org>
2023-10-12 01:04:57 -0400

    Add basic fast api

---
+++ main.py
@@ -0,0 +1,21 @@
+from typing import Union
+
+from fastapi import FastAPI
+
+import csv
+
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    with open("data.csv", "a") as data:
+        data_reader = csv.reader(data, delimiter=" ", quotechar="|")
+
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
